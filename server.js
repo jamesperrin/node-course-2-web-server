c000a99c3ca2a59fb17d457af9ef6c8897018a43
James Perrin <countrydawgg@gmail.com>
2018-02-21 11:56:26 -0800

    Created basic Express routes.

--- server.js
+++ server.js
@@ -2,8 +2,23 @@ const express = require('express');
 
 var app = express();
 
-app.get('/', (req, res) => { 
-  res.send('Hello Express!');
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>');
+  res.send({
+    name: 'James',
+    likes: ['Biking', 'Cities']
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    'errorMessage': 'Unable to handle request'
+  });
 });
 
 app.listen(3000);
