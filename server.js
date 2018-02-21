77581300b5062491b63f329e4c53699b28dfff83
James Perrin <countrydawgg@gmail.com>
2018-02-21 12:46:13 -0800

    (1) Created PartialViews. (2) Created Helper function.

--- server.js
+++ server.js
@@ -2,24 +2,30 @@ const express = require('express');
 const hbs = require('hbs');
 
 var app = express();
-const currentYear = new Date().getFullYear();
 
+hbs.registerPartials(`${__dirname}/views/partials`);
 app.set('view engine', 'hbs');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
+
+hbs.registerHelper('getCurrentYear', () => { 
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => { 
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
-    welcomeMessage: 'Hello, your welcome!',
-    currentYear: currentYear
+    welcomeMessage: 'Hello, your welcome!'
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
-    currentYear: currentYear
+    pageTitle: 'About Page'
   });
 });
 
